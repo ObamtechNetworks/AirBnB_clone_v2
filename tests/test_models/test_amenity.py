d1ef617a7ef887392a8718132c0e6a8581ede698
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2024-02-21 23:49:11 +0300

    unittests for creating amenities with do_create()

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -39,17 +39,17 @@ class test_create_state(unittest.TestCase):
             except FileNotFoundError:
                 pass
 
-            def test_create_amenity(self):
-                hbnb_command = HBNBCommand()
-                file_storage = FileStorage()
-                command = "Amenity name='Swimming_Pool'"
-                hbnb_command.do_create(command)
-                #check states
-                file_storage.reload()
-                total_amenities = file_storage.all(Amenity)
-                self.assertNotEqual(total_amenities, {}, "No Amenity objects found in storage after do_create function.")
-                self.assertEqual(len(total_amenities), 1, "Incorrect number of amenities in the new Amenity object.")
-                #check details of created amenities
-                new_amenities = next(iter(total_amenities.values()), None)
-                self.assertIsNotNone(new_amenities, "New Amenity not found in storage.")
-                self.assertEqual(new_state.name.strip("'"), 'Swimming Pool', "Incorrect state name in the new State object.")
+        def test_create_amenity(self):
+            hbnb_command = HBNBCommand()
+            file_storage = FileStorage()
+            command = "Amenity name='Swimming_Pool'"
+            hbnb_command.do_create(command)
+            #check states
+            file_storage.reload()
+            total_amenities = file_storage.all(Amenity)
+            self.assertNotEqual(total_amenities, {}, "No Amenity objects found in storage after do_create function.")
+            self.assertEqual(len(total_amenities), 1, "Incorrect number of amenities in the new Amenity object.")
+            #check details of created amenities
+            new_amenities = next(iter(total_amenities.values()), None)
+            self.assertIsNotNone(new_amenities, "New Amenity not found in storage.")
+            self.assertEqual(new_state.name.strip("'"), 'Swimming Pool', "Incorrect state name in the new State object.")

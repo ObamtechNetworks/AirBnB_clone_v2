c3cc847fa90258f70648615e3a4c8d6be78e52d1
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2024-02-22 06:08:34 +0300

    unittest for Review object creation

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 """ tests cases for the review model"""
+import json
+import os
+import unittest
+from console import HBNBCommand
+from models.engine.file_storage import FileStorage
+from unittest.mock import patch
+from io import StringIO
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
 
@@ -27,3 +34,31 @@ class test_review(test_basemodel):
         """ tests texts"""
         new = self.value()
         self.assertEqual(type(new.text), str)
+
+class test_create_review(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+        cls.storage = FileStorage()
+        cls.storage.reload()
+
+    @classmethod
+    def tearDownClass(cls):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+
+    def test_create_review(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("create Review"))
+            self.assertLess(0, len(output.getvalue().strip()))
+            test_str = "Review.{}".format(output.getvalue().strip())
+            self.assertIn(test_str, self.storage.all().keys())

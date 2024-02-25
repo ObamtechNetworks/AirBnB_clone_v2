8850b2ea09a7b7d62df893ddb23c6d206e0519dc
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-02-25 03:49:28 +0100

    hopefully

--- models/user.py
+++ models/user.py
@@ -11,13 +11,14 @@ class User(BaseModel, Base):
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128))
-        last_name = Column(String(128))
+        first_name = Column(String(128, nullable=True))
+        last_name = Column(String(128, nullable=True))
     else:
         email = ''
         password = ''
         first_name = ''
         last_name = ''
+    
     def __init__(self, *args, **kwargs):
-        """initializes the state model """
+        """initializes user """
         super().__init__(*args, **kwargs)

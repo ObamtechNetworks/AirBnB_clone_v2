2ccb3a582251dc143e6706ecf88cda9e6c9d3671
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-03-25 01:25:05 +0100

    routing with variables

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+A script that starts a flask web application
+Web application must be listening on 0.0.0.0, port 5000
+Routes
+    /: display "Hello HBNB!"
+    /hbnb: display "HBNB"
+    /c/<text>: display "C" follwed by the value of the text
+    variable (replaces underscores with space
+    Must use the option- strict_slashes=False
+"""
+
+# import modules
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """returns hello hbnb!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """returns 'hbnb'"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_C_with_texts(text):
+    """display 'C' followed by the value of the text variable"""
+    if "_" in text:
+        text = text.replace('_', ' ')
+    return f"C {escape(text)}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
